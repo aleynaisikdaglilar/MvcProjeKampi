52f310b475153b5d776868988f92df096d004cdc
Aleyna Işıkdağlılar <aleynaisikdaglilar@gmail.com>
2021-05-07 04:53:25 +0300

    Add project files.

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,51 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        //GenericRepository<Category> repo = new GenericRepository<Category>();
+
+        //public List<Category> GetAllBl()
+        //{
+        //    return repo.List();
+        //}
+        //public void CategoryAddBl(Category p)
+        //{
+        //    if (p.CategoryName == "" || p.CategoryName.Length <= 3 ||
+        //        p.CategoryName.Length >= 51 || p.CategoryDescription == "")
+        //    {
+        //        //hata mesajı
+        //    }
+        //    else
+        //    {
+        //        repo.Insert(p);
+        //    }
+        //}
+
+        ICategoryDal _categoryDal;
+
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            _categoryDal = categoryDal;
+        }
+
+        public void CategoryAdd(Category category)
+        {
+            _categoryDal.Insert(category);
+        }
+
+        public List<Category> GetList()
+        {
+            return _categoryDal.List();
+        }
+    }
+}
